7f764c10cc0671c93c57e8fd71d00f7f5893e4de
Vikram Ramavarapu <vikramr2@illinois.edu>
2025-06-04 18:34:42 -0500

    better embedder

--- tests/dna_embed_test.py
+++ tests/dna_embed_test.py
@@ -1,23 +1,36 @@
-import os
+from transformers import AutoTokenizer, AutoModelForMaskedLM
 import torch
-from transformers import AutoTokenizer, AutoModel
 
-# Set multiple environment variables to disable Flash Attention
-os.environ['FLASH_ATTENTION_DISABLE'] = '1'
-os.environ['DISABLE_FLASH_ATTN'] = '1'
-os.environ['USE_FLASH_ATTENTION'] = '0'
-
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-
+# Import the tokenizer and the model
 tokenizer = AutoTokenizer.from_pretrained("InstaDeepAI/nucleotide-transformer-500m-human-ref")
-model = AutoModel.from_pretrained("InstaDeepAI/nucleotide-transformer-500m-human-ref")
+model = AutoModelForMaskedLM.from_pretrained("InstaDeepAI/nucleotide-transformer-500m-human-ref")
 
-# Move model to device
-model = model.to(device)
+# Choose the length to which the input sequences are padded. By default, the 
+# model max length is chosen, but feel free to decrease it as the time taken to 
+# obtain the embeddings increases significantly with it.
+max_length = tokenizer.model_max_length
 
-dna = "ACGTAGCATCGGATCTATCTATCGACACTTGGTTATCGATCTACGAGCATCTCGTTAGC"
-inputs = tokenizer(dna, return_tensors='pt')["input_ids"].to(device)
-hidden_states = model(inputs)[0]
+# Create a dummy dna sequence and tokenize it
+sequences = ["ATTCCGATTCCGATTCCG", "ATTTCTCTCTCTCTCTGAGATCGATCGATCGAT"]
+tokens_ids = tokenizer.batch_encode_plus(sequences, return_tensors="pt", padding="max_length", max_length = max_length)["input_ids"]
 
-embedding_mean = torch.mean(hidden_states[0], dim=0)
-print(embedding_mean.shape)
+# Compute the embeddings
+attention_mask = tokens_ids != tokenizer.pad_token_id
+torch_outs = model(
+    tokens_ids,
+    attention_mask=attention_mask,
+    encoder_attention_mask=attention_mask,
+    output_hidden_states=True
+)
+
+# Compute sequences embeddings
+embeddings = torch_outs['hidden_states'][-1].detach().numpy()
+print(f"Embeddings shape: {embeddings.shape}")
+print(f"Embeddings per token: {embeddings}")
+
+# Add embed dimension axis
+attention_mask = torch.unsqueeze(attention_mask, dim=-1)
+
+# Compute mean embeddings per sequence
+mean_sequence_embeddings = torch.sum(attention_mask*embeddings, axis=-2)/torch.sum(attention_mask, axis=1)
+print(f"Mean sequence embeddings: {mean_sequence_embeddings}")
